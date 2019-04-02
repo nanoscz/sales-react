6610b4f0e4fcac8d21f0f89b74b67ed94be91dd6
fernandocto.scz <fernandocto.scz@gmail.com>
2019-04-02 11:26:17 -0400

    Delete Client

--- src/components/client/index.js
+++ src/components/client/index.js
@@ -1,8 +1,9 @@
 import React, { Fragment } from 'react'
-import { Query } from 'react-apollo'
+import { Query, Mutation } from 'react-apollo'
 import { Link } from 'react-router-dom'
 
 import { CLIENTS_QUERY } from '../../queries'
+import { DELETE_CLIENT } from '../../mutations'
 
 import { ListGroup } from 'react-bootstrap';
 const contact = () => (
@@ -19,12 +20,25 @@ const contact = () => (
                 <div className="row justify-content-between align-items-center">
                   <div className="col-md-8">{item.name} {item.lastName} - {item.company}</div>
                   <div className="col-md-4 d-flex justify-content-end">
-                    <button  className="btn btn-outline-danger mr-2"
-                      onClick={ () => {
-                        console.log(item.id)
-                      }}> 
-                    Delete 
-                    </button>
+                    <Mutation mutation={DELETE_CLIENT}>
+                      {deleteClient => {
+                        const {id} = item
+                        const client = `${item.name} ${item.lastName}`
+                        return (
+                          <button className="btn btn-outline-danger mr-2"
+                            onClick={() => {
+                              console.log(item.id)
+                              if(window.confirm(`Delete the client ${client}`)){
+                                deleteClient({
+                                  variables: {id}
+                                })
+                              }
+                            }}>
+                            Delete
+                          </button>
+                        )
+                      }}
+                    </Mutation>
                     <Link to={`/client/edit/${item.id}`} className="btn btn-outline-primary">Edit</Link>
                   </div>
                 </div>
